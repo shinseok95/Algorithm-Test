8a0d39d103e228b37e29ecefd431876bb1a6671b
shin_seok <pjjpjj2002@naver.com>
2021-04-27 23:22:11 +0900

    21.04.27

--- Bellman-Ford/[1865] 웜홀.py
+++ Bellman-Ford/[1865] 웜홀.py
@@ -1,9 +1,56 @@
 """
 벨만 포드를 모르고 다익스트라로 풀었는데, 풀렸다..
 
-그래서 다시 벨만 포드로 풀어봐야한다..
+그래서 벨만 포드로 다시 풀어보았고, 이번 문제가 벨만 포드 알고리즘을 활용하여 음의 사이클이 존재하는지 확인하는 문제라는 것을 알 수 있었다.
+
+기존의 다익스트라의 경우, N번 함수를 호출했어야하는 반면 벨만 포드는 딱 한번만 함수를 호출하면 됐었다.
+
+다음부터 음의 값이 있는 그래프 문제, 음의 사이클을 찾는 문제는 벨만 포드로 접근해야겠다.
 """
 
+import sys
+
+def bf(n):
+  distance[n][n] = 0
+
+  for i in range(N):
+    for j in range(len(edges)):
+
+      cur = edges[j][0]
+      next_node = edges[j][1]
+      cost = edges[j][2]
+
+      if distance[next_node] != INF and distance[n][next_node] > distance[n][cur] + cost:
+        distance[n][next_node] = distance[n][cur] + cost
+
+        if i == N - 1:
+          return True
+
+  return False
+
+T = int(sys.stdin.readline())
+INF = int(1e9)
+
+for _ in range(T):
+  N, M, W = map(int, sys.stdin.readline().split())
+  edges = []
+  distance = [[INF] * (N + 1) for _ in range(N + 1)]
+
+  for _ in range(M):
+    s, e, t = map(int, sys.stdin.readline().split())
+    edges.append((s, e, t))
+    edges.append((e, s, t))
+
+  for _ in range(W):
+    s, e, t = map(int, sys.stdin.readline().split())
+    edges.append((s, e, t * -1))
+
+  if bf(1):
+    print('YES')
+  else:
+    print('NO')
+
+"""
 import sys
 from heapq import heappush,heappop
 
@@ -69,3 +116,4 @@ for _ in range(T):
     print('YES')
   else:
     print('NO')
+"""
